1c1bd8239d010111283944812a63523fa98b0aa6
Alex Pearson <alex@alexpear.com>
2018-04-21 16:09:49 -0500

    :construction: Set up component model for VDOM diffing

--- index.js
+++ index.js
@@ -8,8 +8,6 @@ class Store {
     const action = await actionCreator()
     const newState = action(this.state)
 
-    console.log('click heard, new state is', newState)
-
     this.setState(newState)
   }
 
@@ -23,9 +21,69 @@ class Store {
   }
 }
 
+class Component {
+  constructor(
+    element = 'div',
+    template = () => '',
+    attributes = [],
+    events = []
+  ){
+    this.element = element
+    this.template = template
+    this.attributes = attributes
+    this.events = events
+  }
+
+  createNode(state){
+    const node = document.createElement(this.element)
+
+    this.attributes.forEach(
+      attribute => node.setAttribute(...Object.entries(attribute))
+    )
+
+    this.events.forEach(
+      event => node.addEventListener(...Object.entries(event))
+    )
+
+    node.textContent = this.template(state)
+
+    return node
+  }
+}
+
+const incrementCount = async () => state = ({ ...state, count: state.cout + 1 })
+const decrementCount = async () => {
+  const reducer = await new Promise(resolve => setTimeout(
+    () => resolve(state => ({ ...state, count: state.count - 1 })),
+    3000
+  ))
+
+  return reducer
+}
 
 const ROOT = document.getElementById('root')
 const store = new Store({ count: 0 })
+
+const title = new Component(
+  'h1',
+  () => 'Click the buttons below!',
+)
+const incrementButton = new Component(
+  'button',
+  () => '+',
+  [{ type: 'button' }],
+  [{ click: incrementCount}],
+)
+const decrementButton = new Component(
+  'button',
+  () => '-',
+  [{ type: 'button' }],
+  [{ click: decrementCount }]
+)
+const counter = new Component(
+  'p',
+  state => `Count: ${ state.count }`,
+)
 
 const view = state => ({
   html: `
@@ -46,7 +104,12 @@ const view = state => ({
       event: 'click',
       selector: '#decrement',
       handler: async function decrementCount(){
-        return state => ({ ...state, count: state.count - 1 })
+        const reducer = await new Promise(resolve => setTimeout(
+          () => resolve(state => ({ ...state, count: state.count - 1 })),
+          3000
+        ))
+
+        return reducer
       }
     }
   ]
